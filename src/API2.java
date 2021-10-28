b761a9acc4f914905a10651b926ee24d5fbe1ffc
C <colbyackerman@gmail.com>
2021-10-28 12:39:29 -0500

    update fetch function, switched back to usda api

--- src/API2.java
+++ src/API2.java
@@ -1,9 +1,14 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.swing.*;
 import java.awt.*;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class API2 extends JPanel {
 
@@ -80,22 +85,85 @@ public class API2 extends JPanel {
 
     public static void fetchResultsFromNutritionAPIOnline (String query) {
 
-        String url = "https://trackapi.nutritionix.com/v2/search/instant?query=" + query;
-        String apikey = "716eebfff20e3e750f25fcbd9e860ceb";
-        String apiid = "0e1f918f";
+        String apikey = "fYwLcMgU9oUkNPRX8lVknkuwSWDehaqhoa9mXSAd";
+        String url = "https://api.nal.usda.gov/fdc/v1/foods/search?query=" + query + "&pageSize=2&api_key=" + apikey;
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create (url))
-                .header ("Content-Type", "application/json")
-                .header("x-app-key", apikey)
-                .header("x-app-id", apiid)
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
                 .build();
 
-        client.sendAsync (request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString(Charset.defaultCharset()));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (response == null) {
+            System.out.println ("api response is null");
+            return;
+        }
+
+//        System.out.println(response.body());
+
+        JSONObject jo = new JSONObject(response.body());
+        JSONArray results = jo.getJSONArray("foods");
+//        System.out.println(results.toString());
+        JSONObject first = new JSONObject(results.get(0).toString());
+//        System.out.println(first.toString());
+        JSONArray nutrients = new JSONArray(first.getJSONArray("foodNutrients"));
+//        System.out.println (nutrients.toString());
+        int numNutrients = nutrients.length();
+
+        ArrayList<String> info = new ArrayList<>(10);
+        for (int i = 0; i < numNutrients; i++) {
+
+            JSONObject nutrient = new JSONObject(nutrients.get(i).toString());
+
+            String nutrientName = nutrient.get("nutrientName").toString();
+            String unitName = nutrient.get("unitName").toString();
+            String value = nutrient.get("value").toString();
+
+            if (nutrientName.equals("Total lipid (fat)")) {
+                nutrientName = "Fat";
+            }
+            else if (nutrientName.equals("Carbohydrate, by difference")) {
+                nutrientName = "Carbs";
+            }
+            else if (nutrientName.equals("Energy")) {
+                nutrientName = "";
+            }
+            else if (nutrientName.equals("Sugars, total including NLEA")) {
+                nutrientName = "Sugar";
+            }
+            else if (nutrientName.equals("Fiber, total dietary")) {
+                nutrientName = "Fiber";
+            }
+            else if (nutrientName.equals("Sodium, Na")) {
+                nutrientName = "Sodium";
+            }
+            else if (nutrientName.equals("Fatty acids, total trans")) {
+                continue;
+            }
+            else if (nutrientName.equals("Fatty acids, total saturated")) {
+                continue;
+            }
+
+            if (unitName.equals("KCAL")) {
+                info.add (value + " cal");
+                continue;
+            }
+
+            info.add (nutrientName + " " + value + unitName);
+        }
+
+        for (String infoItem : info) {
+            System.out.println (infoItem);
+        }
 
     }
 
